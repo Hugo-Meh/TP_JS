30b8c5dab30a1bb4e2f288407f152044ca7546a0
Mohamed Nefzi <mnefzi@isimtl.local>
2017-08-01 19:48:46 -0400

    travail momo mardi 01/08

--- script/main.js
+++ script/main.js
@@ -1,47 +1,92 @@
 'use strict';
 // when DOM ready
-const INSER_DONNEE="<tr><td><input type='radio' name='donne' ></td><td class='long'>" +
-    "</td><td class='lat'></td><td class='user'></td><td class='date'></td></tr>";
-let id_tab=$("#donnee_enregistree");
-let button_table=$("#chercher_coord");
-   //constructeur des coordonnees GPS
-    function Releve(lat,long,data) {
-    this.lat=lat;
-    this.long=long;
-    this.data=data;
-    this.date=Date.now();
+const INSER_DONNEE = "<tr><td><input type='radio' name='donne' ></td><td class='long'>(long)" +
+    "</td><td class='lat'>(lat)</td><td class='user'>(user)</td><td class='date'>(date)</td></tr>";
+let id_tab = $("#donnee_enregistree");
+let button_table = $("#chercher_coord");
+let ls = localStorage;
 
+function date() {
+    let date = new Date();
+    console.log(date.toDateString());
+    date = date.toDateString();
+    return date;
+}
+
+//constructeur des coordonnees GPS
+function Releve(lat, long, data) {
+    this.lat  = lat;
+    this.long = long;
+    this.data = data;
+    this.date = date();
 }
 
 //ajouter des Coordonnes dans un tableau
-function ajouter_coord(donnee){
-    id_tab.append(INSER_DONNEE);
-    id_tab.find('.long').last().text(donnee.long);
-    id_tab.find('.lat').last().text(donnee.lat);
-    id_tab.find('.user').last().text(donnee.data);
-    id_tab.find('.date').last().text(donnee.date);
+function ajouter_coord(donnee) {
+    let markup = INSER_DONNEE
+        .replace("(long)", donnee.long)
+        .replace("(lat)", donnee.lat)
+        .replace("(user)", donnee.data)
+        .replace("(date)", donnee.date)
+    $(markup).appendTo(id_tab);
 }
-
-
 
 
 //aller sur une addresse à partir de coordonne gps choisi sur un tableau
-function recuperer_coord_gps(){
-    let cont_parent=id_tab.find("input:checked").parent().parent();
-    let lat=parseFloat(cont_parent.find(".lat").text());
-    let lng=parseFloat(cont_parent.find(".long").text());
-    console.log("fonction recuperer touche",{lat:lat,lng:lng})
-    return{lat:lat,lng:lng};
+function recuperer_coord_gps() {
+    let cont_parent = id_tab.find("input:checked").parent().parent();
+    let lat = parseFloat(cont_parent.find(".lat").text());
+    let lng = parseFloat(cont_parent.find(".long").text());
+    console.log("fonction recuperer touche", {lat: lat, lng: lng})
+    return {lat: lat, lng: lng};
 
 }
 
-//fonction sauvegarde au local storage
 
-$(window).on('unload', function(){
-    console.log("fermer la page");
+$(window).on('unload', function () {
+    sauvegarder_storage();
 });
 
+//fonction sauvegarde au local storage
 
+function sauvegarder_storage() {
+    let tab_tr = id_tab.children("tr");
+    let data_storage = [];
+    ls.clear();
 
+    if (tab_tr.length > 0) {
+        tab_tr.each(function (index, elem) {
+            data_storage.push({
+                "long": $(elem).children(".long").text(),
+                "lat": $(elem).children(".lat").text(),
+                "data": $(elem).children(".user").text(),
+                "date": $(elem).children(".date").text(),
+            })
+        })
+    }
+    ls.setItem("list_data", JSON.stringify(data_storage));
+}
 
+// remplir le tableau des donnees avec les données enregistre dans le storage
+function charger_storage() {
+    let data_storage = ls.getItem("list_data");
 
+    if (data_storage !== null) {
+
+        data_storage = JSON.parse(data_storage);
+        id_tab.empty();
+        for (let i = 0; i <data_storage.length; i++) {
+            console.log(data_storage[i]);
+            ajouter_coord(data_storage[i]);
+        }
+    }
+}
+/*if(ls.getItem("data_list")!==null)
+    $("#actualiser").desabled=true;
+else $("#actualiser").desabled=false;
+*/
+
+$("#actualiser").on("click", function () {
+
+    charger_storage();
+})
--- script/map.js
+++ script/map.js
@@ -2,7 +2,7 @@
 let map, infoWindow;
 
 function initMap() {
-    console.log("initmaps");
+    console.log("initmaps")
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 45.544, lng:-73.640},
         zoom: 8,
@@ -10,11 +10,11 @@ function initMap() {
     });
     infoWindow = new google.maps.InfoWindow;
 
-    $(function(){
 // ajoute les coordonnee d'un click donnée au tableau
     let tab_coord = [];
     google.maps.event.addListener(map, 'click', function (event) {
         alert("Latitude: " + event.latLng.lat() + " " + ", longitude: " + event.latLng.lng());
+
 
         let donnee = new Releve(event.latLng.lat(), event.latLng.lng(), "donnee");
         console.log(donnee);
@@ -23,6 +23,7 @@ function initMap() {
         //ajout de chaque nouveau coordonnee dans un table dans le html
        ajouter_coord(donnee);
     });
+
 
 // Try HTML5 geolocation.
     if (navigator.geolocation) {
@@ -53,19 +54,24 @@ function initMap() {
         infoWindow.open(map);
     }
 
+
+
+
+
     document.getElementById("chercher_coord").onclick=function(){
+
         let pos=recuperer_coord_gps();
+
+
         infoWindow.setPosition(pos);
         console.log(pos);
         infoWindow.setContent('Location found.');
         infoWindow.open(map);
         map.setCenter(pos);
 
-    }, function () {
-        handleLocationError(true, infoWindow, map.getCenter());
+
     };
-    })
-//fin a ne pas depasser
+
+
+    //fin a ne pas depasser
 }
-
-
