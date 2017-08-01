81ab18e25e42342943de185fa00201254e08eac1
Mohamed Nefzi <mnefzi@isimtl.local>
2017-08-01 12:28:42 -0400

    ajout initial momo

--- script/main.js
+++ script/main.js
@@ -1,4 +1,47 @@
 'use strict';
 // when DOM ready
+const INSER_DONNEE="<tr><td><input type='radio' name='donne' ></td><td class='long'>" +
+    "</td><td class='lat'></td><td class='user'></td><td class='date'></td></tr>";
+let id_tab=$("#donnee_enregistree");
+let button_table=$("#chercher_coord");
+   //constructeur des coordonnees GPS
+    function Releve(lat,long,data) {
+    this.lat=lat;
+    this.long=long;
+    this.data=data;
+    this.date=Date.now();
+
+}
+
+//ajouter des Coordonnes dans un tableau
+function ajouter_coord(donnee){
+    id_tab.append(INSER_DONNEE);
+    id_tab.find('.long').last().text(donnee.long);
+    id_tab.find('.lat').last().text(donnee.lat);
+    id_tab.find('.user').last().text(donnee.data);
+    id_tab.find('.date').last().text(donnee.date);
+}
+
+
+
+
+//aller sur une addresse à partir de coordonne gps choisi sur un tableau
+function recuperer_coord_gps(){
+    let cont_parent=id_tab.find("input:checked").parent().parent();
+    let lat=parseFloat(cont_parent.find(".lat").text());
+    let lng=parseFloat(cont_parent.find(".long").text());
+    console.log("fonction recuperer touche",{lat:lat,lng:lng})
+    return{lat:lat,lng:lng};
+
+}
+
+//fonction sauvegarde au local storage
+
+$(window).on('unload', function(){
+    console.log("fermer la page");
+});
+
+
+
 
 
--- script/map.js
+++ script/map.js
@@ -1,16 +1,32 @@
 'use strict';
-    let map,infoWindow;
-    function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat: -34.397, lng: 150.644},
-            zoom: 8
-        });
-        infoWindow = new google.maps.InfoWindow;
-    }
+let map, infoWindow;
+
+function initMap() {
+    console.log("initmaps");
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 45.544, lng:-73.640},
+        zoom: 8,
+        mapTypeId: google.maps.MapTypeId.HYBRID,
+    });
+    infoWindow = new google.maps.InfoWindow;
+
+    $(function(){
+// ajoute les coordonnee d'un click donnée au tableau
+    let tab_coord = [];
+    google.maps.event.addListener(map, 'click', function (event) {
+        alert("Latitude: " + event.latLng.lat() + " " + ", longitude: " + event.latLng.lng());
+
+        let donnee = new Releve(event.latLng.lat(), event.latLng.lng(), "donnee");
+        console.log(donnee);
+        //sauvegarder les element dans un tableau
+        tab_coord.push(donnee) ;
+        //ajout de chaque nouveau coordonnee dans un table dans le html
+       ajouter_coord(donnee);
+    });
 
 // Try HTML5 geolocation.
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
+        navigator.geolocation.getCurrentPosition(function (position) {
             let pos = {
                 lat: position.coords.latitude,
                 lng: position.coords.longitude
@@ -21,7 +37,7 @@
             infoWindow.setContent('Location found.');
             infoWindow.open(map);
             map.setCenter(pos);
-        }, function() {
+        }, function () {
             handleLocationError(true, infoWindow, map.getCenter());
         });
     } else {
@@ -36,4 +52,20 @@
             'Error: Your browser doesn\'t support geolocation.');
         infoWindow.open(map);
     }
+
+    document.getElementById("chercher_coord").onclick=function(){
+        let pos=recuperer_coord_gps();
+        infoWindow.setPosition(pos);
+        console.log(pos);
+        infoWindow.setContent('Location found.');
+        infoWindow.open(map);
+        map.setCenter(pos);
+
+    }, function () {
+        handleLocationError(true, infoWindow, map.getCenter());
+    };
+    })
+//fin a ne pas depasser
+}
+
 
